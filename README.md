688d6d6bd15aa9c9c46e5a2276975a7751e5b193
lurapozo <lurapozo@espol.edu.ec>
2024-02-19 23:10:07 -0500

    Web server with model prediction

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Gem Prediction
 
-Learning how to use FastApi with Jira2 templates and poetry. 
+Learning how to use FastApi with Jira2 templates, AlpineJS and poetry. 
 In this project I'll be comparing multiple regression models to predict the prices of a Zirconia gem based on its attributes.
 
 Data obtained from [Kaggle](https://www.kaggle.com/datasets/colearninglounge/gemstone-price-prediction/data).
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+import joblib
 
 app = FastAPI()
 
@@ -9,7 +10,27 @@ templates = Jinja2Templates(directory="templates")
 
 @app.get("/", response_class=HTMLResponse)
 async def read_item(request: Request):
-    id = 1
     return templates.TemplateResponse(
-        request=request, name="gem.html"
+        request=request, name="gem.html",
+        context={"models":
+                     [{"id": 1, "model": "SVR"},
+                      {"id": 2, "model": "Linear Regression"},
+                      {"id": 3, "model": "Random Forest Classifier"}]}
+    )
+
+@app.get("/prediction", response_class=HTMLResponse)
+async def predition(
+        request: Request,
+                    carat: float, depth: float, table: float, model: str):
+    if model == "SVR":
+        reg = joblib.load("./models/svr_no_cat.z")
+        pred = reg.predict([[carat, depth, table]])
+    elif model == "Linear Regression":
+        reg = joblib.load("./models/lr_no_cat.z")
+        pred = reg.predict([[carat, depth, table]])
+    else: # random forest
+        reg = joblib.load("./models/rf_no_cat.z")
+        pred = reg.predict([[carat, depth, table]])
+    return templates.TemplateResponse(
+        request=request, name="prediction.html", context={"model": model, "price": pred[0]}
     )
